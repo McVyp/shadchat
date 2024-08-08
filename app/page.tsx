8f96b4e3957040180e9a835108c79e768e63a76e
McVyp <lyeclyde@gmail.com>
2024-08-08 18:35:13 +0900

    add zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/ui/header";
+import InitUser from "@/lib/store/InitUser";
 import { createServer } from "@/lib/supabase/server";
 import React from "react";
 
@@ -6,10 +7,13 @@ export default async function Page() {
   const supbase = createServer();
   const { data } = await supbase.auth.getSession();
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
-      <div className="h-full border rounded-md">
-        <Header user={data.session?.user} />
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className="h-full border rounded-md">
+          <Header user={data.session?.user} />
+        </div>
       </div>
-    </div>
+      <InitUser user={data.session?.user}/>
+    </>
   );
 }
