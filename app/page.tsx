a4dc7e75cc57882257045125600ba14aeb89b57f
McVyp <lyeclyde@gmail.com>
2024-08-08 16:41:15 +0900

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-"use client";
 import Header from "@/components/ui/header";
+import { createServer } from "@/lib/supabase/server";
 import React from "react";
 
-export default function Page() {
+export default async function Page() {
+  const supbase = createServer();
+  const { data } = await supbase.auth.getSession();
   return (
     <div className="max-w-3xl mx-auto md:py-10 h-screen">
       <div className="h-full border rounded-md">
-        <Header />
+        <Header user={data.session?.user} />
       </div>
     </div>
   );
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,17 +1,27 @@
+"use client";
 import React from "react";
 import { Button } from "./button";
 import { createClient } from "@/lib/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
 
-export default function Header() {
-  const handlLogin = () => {
+export default function Header({ user }: { user: User | undefined }) {
+  const router = useRouter();
+  const handleLogin = async () => {
     const supabase = createClient();
     supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "/auth/callback",
+        redirectTo: location.origin + "/auth/callback",
       },
     });
   };
+  const handleLogout = async () => {
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+  console.log(user);
   return (
     <div className="h-20">
       <div className="p-4 border-b flex items-center justify-between">
@@ -22,7 +32,11 @@ export default function Header() {
             <h1 className="text-sm text-gray-300">2 onlines</h1>
           </div>
         </div>
-        <Button onClick={handlLogin}>Login</Button>
+        {!user ? (
+          <Button onClick={handleLogin}>Login</Button>
+        ) : (
+          <Button onClick={handleLogout}>Logout</Button>
+        )}
       </div>
     </div>
   );
