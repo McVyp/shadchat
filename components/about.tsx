e297ab0f38c8ddb82ee1c4bc8c98177d54f11aaa
McVyp <lyeclyde@gmail.com>
2024-08-26 12:16:31 +0900

    add logout UI

---
+++ components/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <div className="text-center space-y-5">
+        <h1 className="text-3xl font-mplus font-bold">Welcome to Chat</h1>
+        <p className="w-96">Login to sned messages</p>
+      </div>
+    </div>
+  );
+}
