d9c41b64582bc8f1407418cdd8012d1291637da1
McVyp <lyeclyde@gmail.com>
2025-01-28 15:19:20 +0545

    fix typo

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className="flex-1 flex items-center justify-center">
       <div className="text-center space-y-5">
         <h1 className="text-3xl font-mplus font-bold">Welcome to Chat</h1>
-        <p className="w-96">Login to sned messages</p>
+        <p className="w-96">Login to send messages</p>
       </div>
     </div>
   );
